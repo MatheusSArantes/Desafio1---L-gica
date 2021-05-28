2ba4fb232e2bd0ae3a247f1e7bf121acd468c9bd
“MatheusSArantes” <matheus.sodre.arantes@gmail.com>
2021-05-28 10:48:06 -0300

    Desafio1

---
+++ mod.js
@@ -0,0 +1,11 @@
+let nome = Matheus
+let idade = 17
+let ano = 2021
+
+console.log("Meu ano de nascimento é " + (ano - idade))
+
+let souhumano = true
+let PrimeiroBimestre = 7
+let SegundoBimestre = 8
+
+console.log(PrimeiroBimestre + SegundoBimestre)
